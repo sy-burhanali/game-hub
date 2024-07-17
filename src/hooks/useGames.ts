90508ce56230231ce3e9a2a88405e7cb4e7ab475
Syed Burhan Ali Shah <admin@rkg3.onmicrosoft.com>
2024-07-17 22:40:37 +0500

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,24 +24,30 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
   
     // using effect hook to send a fetch request to the backend
     useEffect(() => {
         const controller = new AbortController();
 
+        setLoading(true);
       apiClient
         .get<FetchGamesResponse>("/games", { signal: controller.signal })
-        .then((res) => setGames(res.data.results))
+        .then((res) => {
+          setGames(res.data.results);
+          setLoading(false);
+        })
         .catch((err) => {
             // used CancelledError as it will send get request twice in StrictMode and first request will be cancelled always
             if (err instanceof CanceledError) return;
             setError(err.message)
+            setLoading(false); 
         });
 
         return () => controller.abort();
     }, []);
 
-    return { games, error };
+    return { games, error, isLoading };
 }
 
 export default useGames;
